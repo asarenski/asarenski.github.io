85ffc9ce833aa2cbf7fb042a99e783beddd33e28
adam sarenski <asarenski@zipcar.com>
2018-11-19 16:07:17 -0500

    changed colors of site

--- src/constants/colorPalette.js
+++ src/constants/colorPalette.js
@@ -7,16 +7,20 @@
 
 // https://flatuicolors.com/palette/us
 // const soothingBreeze = '#b2bec3' // gray
-const cityLights = '#dfe6e9' // gray
+// const cityLights = '#dfe6e9' // gray
 // const americanRiver = '#636e72' // gray
 const electronBlue = '#0984e3' // blue
-const greenDarnerTail = '#74b9ff' // blue
 // const draculaOrchid = '#2d3436' // black
 
+// color adobe
+// https://color.adobe.com/create/color-wheel/?base=2&rule=Analogous&selected=1&name=My%20Color%20Theme&mode=rgb&rgbvalues=0.8359421278561068,0.7354085324044554,1,0.884169090537485,0.8839831608196197,0.91,0.6842544668494852,0.795660365555102,1,0.6397121542264083,0.8323097162055173,0.91,0.6314688576137668,1,0.9713463141953877&swatchOrder=0,1,2,3,4
+const wildRobinLight = 'rgba(174,203,255,0.4)'
+const linkWeed = '#E1E1E8' // gray green
+
 export default {
-  header: cityLights,
-  jumbotron: greenDarnerTail,
+  header: 'white',
+  jumbotron: wildRobinLight,
   link: electronBlue,
   border: 'rgba(0,0,0,0.125)',
-  card: cityLights,
+  card: linkWeed,
 }
