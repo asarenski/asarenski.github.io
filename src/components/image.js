2cf3547afe17723190e90a989de0ee6a65383d6a
Adam Sarenski <asarenski@gmail.com>
2025-04-26 09:24:54 -0400

    upgrade all gatsby

--- src/components/image.js
+++ src/components/image.js
@@ -1,32 +1,15 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `StaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.app/gatsby-image
- * - `StaticQuery`: https://gatsby.app/staticquery
- */
+import { StaticImage } from "gatsby-plugin-image"
 
 const Image = ({style}) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "headshot_white_small.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => <Img style={style} fluid={data.placeholderImage.childImageSharp.fluid} />}
+  <StaticImage
+    src="../images/headshot_white_small.jpg"
+    loading="eager"
+    width={360}
+    quality={60}
+    formats={["auto", "webp", "avif"]}
+    alt="asarenski headshot"
+    style={style}
   />
 )
 export default Image
