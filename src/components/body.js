0a3c2e81e5e91677da932cc1ad5f59c174ecf6da
Adam Sarenski <asarenski@gmail.com>
2025-04-26 09:27:08 -0400

    cleanup projects shown

--- src/components/body.js
+++ src/components/body.js
@@ -2,19 +2,14 @@ import React from 'react'
 import HeaderWithLink from './headerWithLink';
 import Card from './card';
 
-const EXAMPLE_PROJ = 'Example Projects'
-const JSFIDDLE = 'JSFiddle'
 const TOOLS = 'Tools'
-const PROFILES = 'Contact'
+const CONTACT = 'Contact'
 
 const links = {
-  [EXAMPLE_PROJ]: [{ href: 'https://asarenski.github.io/angular1Example', text: 'Angular1 Example' }],
-  [JSFIDDLE]: [{ href: 'https://jsfiddle.net/user/asarenski/fiddles', text: 'JSFiddle' }],
   [TOOLS]: [
     { href: 'https://asarenski.github.io/browser_version_alert', text: 'browser version alert' },
-    { href: 'https://github.com/asarenski/gdax-crypto-tax-js', text: 'tax calculator gdax' },
   ],
-  [PROFILES]: [
+  [CONTACT]: [
     { href: 'https://github.com/asarenski', text: 'github' },
     { href: 'https://www.linkedin.com/in/adamsarenski/', text: 'linkedIn' },
   ],
